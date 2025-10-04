29e72777725161881c48da550faa55dcaeac4e65
Ahmad Rizal <ahmaddrizalul@gmail.com>
2025-10-04 21:46:27 +0700

    add endpoint redirect url

--- api/routes/getUrl.go
+++ api/routes/getUrl.go
@@ -18,3 +18,15 @@ func GetByShortID(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"url": val})
 }
+
+func RedirectByShortID(c *gin.Context) {
+	shortID := c.Param("shortID")
+
+	val, err := database.Client.Get(database.Ctx, shortID).Result()
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
+		return
+	}
+
+	c.Redirect(http.StatusMovedPermanently, val)
+}
